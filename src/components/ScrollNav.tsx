299b75261b119d7c73075f377b5ef55017056715
youserstack <youserstack@gmail.com>
2024-05-22 15:56:36 +0900

    update

--- src/components/ScrollNav.tsx
+++ src/components/ScrollNav.tsx
@@ -16,11 +16,13 @@ export default function ScrollNav(
     // category가 일치한 경우, sub1-categories를 활성화한다.
     const categories = document.querySelectorAll(".category") as NodeListOf<HTMLElement>;
     categories.forEach((category: HTMLElement) => {
-      const span = category.querySelector("span") as HTMLElement;
-      const ulElement = category.querySelector("ul") as HTMLElement; // 서브카테고리 엘리먼트
+      const spanElement = category.querySelector("span") as HTMLElement;
+      const buttonElement = category.querySelector("button") as HTMLElement;
+      const ulElement = category.querySelector("ul") as HTMLElement;
       if (!ulElement) return;
-      if (span.textContent === params.category[0]) {
-        ulElement.style.maxHeight = "100vh"; // 서브카테고리 활성화
+      if (spanElement.textContent === params.category[0]) {
+        buttonElement.style.transform = "rotate(90deg)"; // 버튼 아이콘 회전
+        ulElement.style.maxHeight = "100vh"; // 서브 카테고리 활성화
         category.setAttribute("data-is-collapsed", "true"); // 돔 상태변경
 
         // sub1Category가 일치한 경우, sub2-categories를 활성화한다.
@@ -28,11 +30,13 @@ export default function ScrollNav(
           ".sub1-category"
         ) as NodeListOf<HTMLElement>;
         sub1Categories.forEach((sub1Category: HTMLElement) => {
-          const sub1Span = sub1Category.querySelector("span") as HTMLElement;
-          const ulElementInSub1 = sub1Category.querySelector("ul") as HTMLElement;
-          if (!ulElementInSub1) return;
-          if (sub1Span.textContent === params.category[1]) {
-            ulElementInSub1.style.maxHeight = "100vh";
+          const buttonElement = sub1Category.querySelector("button") as HTMLElement;
+          const spanElement = sub1Category.querySelector("span") as HTMLElement;
+          const ulElement = sub1Category.querySelector("ul") as HTMLElement;
+          if (!ulElement) return;
+          if (spanElement.textContent === params.category[1]) {
+            buttonElement.style.transform = "rotate(90deg)";
+            ulElement.style.maxHeight = "100vh";
             sub1Category.setAttribute("data-is-collapsed", "true");
           }
         });
@@ -43,26 +47,18 @@ export default function ScrollNav(
   const handleClick: MouseEventHandler = (e) => {
     // 클릭이벤트가 발생한 리스트아이템 엘리먼트
     const liElement = e.currentTarget.closest("li") as HTMLElement;
-    const ulElement = liElement.querySelector("ul");
+    const ulElement = liElement.querySelector("ul") as HTMLElement;
+    const buttonElement = liElement.querySelector("button") as HTMLElement;
     const isCollapsed = liElement.getAttribute("data-is-collapsed");
-    console.log({ isCollapsed });
 
     if (ulElement instanceof HTMLUListElement) {
       if (isCollapsed === "false") {
-        console.log("활성화!");
-        // 서브카테고리 엘리먼트 활성화
-        // folding (접힌상태:down) => unfolding (펼친상태:up)
+        buttonElement.style.transform = "rotate(90deg)";
         ulElement.style.maxHeight = "100vh";
-
-        // 상태변경
         liElement.setAttribute("data-is-collapsed", "true");
       } else {
-        console.log("비활성화!");
-        // 서브카테고리 엘리먼트 비활성화
-        // unfolding (펼친상태:up) => folding (접힌상태:down)
         ulElement.style.maxHeight = "0";
-
-        // 상태변경
+        buttonElement.style.transform = "rotate(0)";
         liElement.setAttribute("data-is-collapsed", "false");
       }
     }
@@ -75,12 +71,7 @@ export default function ScrollNav(
         {categories.map((category: any) => {
           const categoryPath = `/categories/${category.name}`;
           return (
-            <li
-              className="category"
-              key={categoryPath}
-              data-is-collapsed={"false"}
-              // data-key={categoryPath}
-            >
+            <li className="category" key={categoryPath} data-is-collapsed={"false"}>
               <Link href={categoryPath} onClick={handleClick}>
                 <span>{category.name}</span>
                 <button onClick={(e) => e.preventDefault()}>
@@ -93,12 +84,7 @@ export default function ScrollNav(
                 {category.sub1Categories?.map((sub1Category: any) => {
                   const categoryPath = `/categories/${category.name}/${sub1Category.name}`;
                   return (
-                    <li
-                      className="sub1-category"
-                      key={categoryPath}
-                      data-is-collapsed={"false"}
-                      // data-key={categoryPath}
-                    >
+                    <li className="sub1-category" key={categoryPath} data-is-collapsed={"false"}>
                       <Link href={categoryPath} onClick={handleClick}>
                         <span>{sub1Category.name}</span>
                         <button onClick={(e) => e.preventDefault()}>
