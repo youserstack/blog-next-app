73929b01c563a956bb4a40baf042feb1f62b0190
youserstack <youserstack@gmail.com>
2024-06-27 17:56:23 +0900

    update

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,15 +2,14 @@ import PostArticle from "@/components/articles/PostArticle";
 import { getPost } from "@/lib/utils/fetchers/getters";
 import "./page.scss";
 
-// export const dynamicParams = false;
-
 export async function generateStaticParams() {
   const { posts } = await fetch(`${process.env.ROOT_URL}/api/static/all-posts`).then((res) =>
     res.json()
   );
   console.log({ posts });
-  return posts;
-  // return posts.map((post: any) => ({ "...id": post._id }));
+
+  // return posts;
+  return posts.map((post: any) => ({ id: post._id }));
 }
 
 export default async function PostId({ params: { id: postId } }: { params: { id: string } }) {
