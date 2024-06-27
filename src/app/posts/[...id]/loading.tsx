f34b0eda6352eb48f01de158a880339f3576648a
youserstack <youserstack@gmail.com>
2024-06-27 15:59:57 +0900

    update

---
+++ src/app/posts/[...id]/loading.tsx
@@ -0,0 +1,11 @@
+// import "./loading.scss";
+
+export default function Loading() {
+  return (
+    <main className="post-id loading">
+      <section>
+        <h1>Server Loading...</h1>
+      </section>
+    </main>
+  );
+}
