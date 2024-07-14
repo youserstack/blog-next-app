d3eb46e4f8f4c9b64f8a502f3ec648db99b7f606
youserstack <youserstack@gmail.com>
2024-07-14 14:02:41 +0900

    update

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,14 +1,18 @@
 import SearchFilter from "@/components/features/SearchFilter";
 import SearchResult from "@/components/articles/SearchResult";
 import { getCategories } from "@/lib/utils/fetchers/getters";
+import { Suspense } from "react";
+import Loading from "@/components/ui/Loading";
 
 export default async function Search() {
   const { categories } = await getCategories();
 
   return (
     <div className="search">
-      <SearchFilter categories={categories} />
-      <SearchResult />
+      <Suspense fallback={<Loading />}>
+        <SearchFilter categories={categories} />
+        <SearchResult />
+      </Suspense>
     </div>
   );
 }
