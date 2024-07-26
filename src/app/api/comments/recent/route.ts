8d07faaf7eba5dd216cae78d1d7e05061695413b
youserstack <youserstack@gmail.com>
2024-07-27 07:58:51 +0900

    update

--- src/app/api/comments/recent/route.ts
+++ src/app/api/comments/recent/route.ts
@@ -2,11 +2,11 @@ import connectDB from "@/lib/config/connectDB";
 import Comment from "@/lib/models/Comment";
 
 export async function GET(request: Request) {
-  // console.log(`\n\x1b[32m[api/comments/recent]:::[GET]\x1b[0m`);
+  console.log(`\n\x1b[32m[api/comments/recent]:::[GET]\x1b[0m`);
   await connectDB();
 
   const comments = await Comment.find().sort({ createdAt: -1 }).limit(5).populate("author");
-  // console.log({ comments });
+  console.log({ comments });
 
   return Response.json({ comments }, { status: 200 });
 }
