89fc65b955acc43dcaed24dd22233e05a3e48335
youserstack <youserstack@gmail.com>
2024-07-27 16:05:51 +0900

    update

--- src/app/api/comments/recent/route.ts
+++ src/app/api/comments/recent/route.ts
@@ -14,7 +14,17 @@ export async function GET(request: Request) {
     console.log(`\n\x1b[32m[api/comments/recent]:::[GET]\x1b[0m`);
     await connectDB(); // DB 연결
 
-    const comments = await Comment.find({}).sort({ createdAt: -1 }).limit(5).populate("author");
+    const comments = await Comment
+      //
+      .find({})
+      .sort({ createdAt: -1 })
+      .limit(5)
+      // .populate("author");
+      .populate({
+        path: "author", // 'author' 필드를 사용하여 User 정보 가져오기
+        select: "name image", // User에서 가져올 필드(이름) 선택
+      });
+
     console.log({ comments });
 
     return Response.json({ comments }, { status: 200 });
