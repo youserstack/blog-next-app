59923d0c61a77b520c4e5298fd63bc83bcdd48b1
youserstack <youserstack@gmail.com>
2024-08-16 15:05:47 +0900

    update

--- src/app/api/warmup/route.ts
+++ src/app/api/warmup/route.ts
@@ -1,6 +1,6 @@
 export const dynamic = "force-dynamic"; // static by default, unless reading the request
 
-export function GET(req: Request) {
+export async function GET() {
   console.log("cron warm up");
   return Response.json({ message: "cron warm up" }, { status: 200 });
 }
