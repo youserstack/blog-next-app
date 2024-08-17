873590f9f1663ed4bbbdfa285671ed86ea6b7329
youserstack <youserstack@gmail.com>
2024-08-17 14:19:48 +0900

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,26 @@
 import type { Metadata } from "next";
 import { Provider } from "@/components/context/Context";
 import { cookies, headers } from "next/headers";
-import Header from "@/components/layout/Header";
+// import Header from "@/components/layout/Header";
 import dynamic from "next/dynamic";
 import { WebVitals } from "@/_components/web-vitals";
 import "./globals.scss";
+
+// server component
+const Header = dynamic(() => import("@/components/layout/Header"), {
+  // loading: () => (
+  //   <div
+  //     style={{
+  //       position: "fixed",
+  //       top: "0",
+  //       left: "0",
+  //       right: "0",
+  //       height: "300px",
+  //       backgroundColor: "green",
+  //     }}
+  //   ></div>
+  // ),
+});
 
 // client components
 const GlobalModal = dynamic(() => import("@/components/modals/GlobalModal"));
