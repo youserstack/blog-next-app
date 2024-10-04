cca89eb376efae568ae26692fd2c40d0566a00c8
youserstack <youserstack@gmail.com>
2024-10-04 13:58:43 +0900

    update

--- src/components/buttons/CommentOptionButton.tsx
+++ src/components/buttons/CommentOptionButton.tsx
@@ -13,30 +13,8 @@ export default function CommentOptionButton({ commentId, postId }: any) {
 
   const handleDeleteComment = async (e: React.MouseEvent<HTMLLIElement>) => {
     e.preventDefault();
-    try {
-      const url = `${process.env.ROOT_URL}/api/comments?postId=${postId}`;
-      const accessToken = localStorage.getItem("accessToken") as string;
-      const result = await deleteComment(commentId, accessToken);
-
-      if (result.error?.code === "ERR_JWT_EXPIRED") {
-        const newAccessToken = await refreshAccessToken();
-        const result = await deleteComment(commentId, newAccessToken);
-
-        if (result.error) throw new Error("토큰갱신 후 댓글삭제요청 실패");
-        console.log("토큰갱신 > 재요청 > 댓글을 삭제하였습니다.");
-        const { deletedComment } = result;
-        console.log({ deletedComment });
-        mutate(url);
-        return;
-      }
-
-      console.log("댓글을 삭제하였습니다.");
-      const { deletedComment } = result;
-      console.log({ deletedComment });
-      mutate(url);
-    } catch (error) {
-      console.error(error);
-    }
+    const url = `${process.env.ROOT_URL}/api/comments?postId=${postId}`;
+    mutate(url);
   };
 
   return (
@@ -47,7 +25,20 @@ export default function CommentOptionButton({ commentId, postId }: any) {
 
       <Popper open={isOpen}>
         <Menu anchorEl={anchorEl} open={isOpen} onClose={() => setAnchorEl(null)}>
-          <MenuItem onClick={handleDeleteComment}>삭제</MenuItem>
+          <MenuItem
+            onClick={async (e) => {
+              e.preventDefault();
+
+              const response = await fetch(`${process.env.ROOT_URL}/api/comments/${commentId}`, {
+                method: "delete",
+              });
+              const data = await response.json();
+
+              console.log("삭제된 댓글", data);
+            }}
+          >
+            삭제
+          </MenuItem>
         </Menu>
       </Popper>
     </div>
